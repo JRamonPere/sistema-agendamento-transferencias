b3f2a8dbb745c639951861d91d762361daede2aa
JRamonPere <joseramonpereira@hotmail.com>
2026-06-20 21:13:20 -0300

    chore: criacao projeto spring boot e java 11

---
+++ backend/src/test/java/br/com/ramon/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ramon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
